1d314c8dd583e3f8a324e194207ba7f4ff2eaa03
yangyifeng <cnyangyifeng@gmail.com>
2016-06-04 01:53:20 +0800

    Initial commit.

---
+++ client/app/core/i18n.js
@@ -0,0 +1,49 @@
+(function () {
+
+    angular.module('app.i18n', ['pascalprecht.translate'])
+        .config(['$translateProvider', i18nConfig])
+        .controller('LangCtrl', ['$scope', '$translate', LangCtrl]);
+
+    function i18nConfig($translateProvider) {
+
+        $translateProvider.useStaticFilesLoader({
+            prefix: 'i18n/',
+            suffix: '.json'
+        });
+
+        $translateProvider.preferredLanguage('zh');
+        $translateProvider.useSanitizeValueStrategy(null);
+    }
+
+    function LangCtrl($scope, $translate) {
+        $scope.lang = '中文';
+        $scope.setLang = setLang;
+        $scope.getFlag = getFlag;
+
+
+        function setLang(lang) {
+            switch (lang) {
+                case 'English':
+                    $translate.use('en');
+                    break;
+                case '中文':
+                    $translate.use('zh');
+                    break;
+            }
+            return $scope.lang = lang;
+        };
+
+        function getFlag() {
+            var lang;
+            lang = $scope.lang;
+            switch (lang) {
+                case 'English':
+                    return 'flags-american';
+                    break;
+                case '中文':
+                    return 'flags-china';
+                    break;
+            }
+        };
+    }
+})();
